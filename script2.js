5ec55685f42b7a0b3f3f1ba7436922f307228094
Natnicha Kotangka <84181541+NatnichaKotangka@users.noreply.github.com>
2022-04-11 02:08:37 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,6 +2,6 @@ function display() {
     var inputName2 = document.getElementById("name2");
     var inputPassword2 = document.getElementById("pass2");
 
-    inputName2.innerHTML = "Username is " + localStorage.getItem("sName");
-    inputPassword2.innerHTML = "Password is " + localStorage.getItem("sPass");
+    inputName2.innerHTML = localStorage.getItem("sName");
+    inputPassword2.innerHTML = localStorage.getItem("sPass");
 }
